37d49b284413735631f0ac1d15331e12304c9e92
oliverson1989@gmail.com <oliverson1989@gmail.com@1633c36a-21c8-e8cd-f5b8-5bfbea1bbbbd>
2013-08-04 14:33:31 +0000

    test

--- trunk/public/index.php
+++ trunk/public/index.php
@@ -1,13 +1,20 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../framework/yii.php';
-$config=dirname(__FILE__).'/../ProtectedShop/config/frontend.php';
+$sd = explode('/',$_SERVER["REQUEST_URI"]);
+echo $_SERVER["REQUEST_URI"];
+if(($sd = explode('/',$_SERVER["REQUEST_URI"])) && $sd[1] == 'admin')
+    echo $sd[1];
+else
+{
+    $yii=dirname(__FILE__).'/../framework/yii.php';
+    $config=dirname(__FILE__).'/../ProtectedShop/config/frontend.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+    defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-require_once($yii);
-Yii::createWebApplication($config)->runEnd('frontend');
+    require_once($yii);
+    Yii::createWebApplication($config)->runEnd('frontend');
+}
