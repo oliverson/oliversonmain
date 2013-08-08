8ae1dfeccd684e5b27e43c04ae317bff08ebd4ac
oliverson1989 <oliverson1989@gmail.com>
2013-08-08 11:14:12 +0000

    update

---
+++ ProtectedShop/messages/backend/vi.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Administrator
+ * Date: 8/8/13
+ * Time: 5:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+return array(
+    'login.page_title'=>'Đăng Nhập',
+);
--- ProtectedShop/modules/backend/views/layouts/main.php
+++ ProtectedShop/modules/backend/views/layouts/main.php
@@ -140,17 +140,7 @@
         </ul>
     </div>
 
-    <div id="login">
-        <form class="form-signin" method="post" action="index.html?lang=en">
-            <h3 class="glyphicons unlock form-signin-heading"><i></i> Sign in</h3>
-            <div class="uniformjs">
-                <input type="text" class="input-block-level" placeholder="Email address">
-                <input type="password" class="input-block-level" placeholder="Password">
-                <label class="checkbox"><input type="checkbox" value="remember-me">Remember me</label>
-            </div>
-            <button class="btn btn-large btn-primary" type="submit">Sign in</button>
-        </form>
-    </div>
+    <?php echo $content; ?>
 
 </div>
 
