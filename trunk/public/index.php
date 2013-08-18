7e9da609117f92f565b72f1387469b55cb7a6a63
oliverson1989@gmail.com <oliverson1989@gmail.com@1633c36a-21c8-e8cd-f5b8-5bfbea1bbbbd>
2013-08-18 06:07:00 +0000

    run

--- trunk/public/index.php
+++ trunk/public/index.php
@@ -1,30 +1,11 @@
 <?php
 
 // change the following paths if necessary
-
-if(($sd = explode('/',$_SERVER["REQUEST_URI"])) && $sd[1] == 'admin')
+if($_SERVER["HTTP_HOST"]!='yii-web3s.local')
 {
-    $yii=dirname(__FILE__).'/../framework/yii.php';
-    $config=dirname(__FILE__).'/../ProtectedShop/config/backend.php';
+    defined('BASE_PATH')
+    || define('BASE_PATH', realpath(dirname(__FILE__) . '/../'));
 
-// remove the following lines when in production mode
-    defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
-    require_once($yii);
-    Yii::createWebApplication($config)->runEnd('backend');
-}
-else
-{
-    $yii=dirname(__FILE__).'/../framework/yii.php';
-    $config=dirname(__FILE__).'/../ProtectedShop/config/frontend.php';
-
-// remove the following lines when in production mode
-    defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
-    require_once($yii);
-    Yii::createWebApplication($config)->runEnd('frontend');
+    include_once BASE_PATH.'/RunApp/Program.php';
+    Program::run();
 }
