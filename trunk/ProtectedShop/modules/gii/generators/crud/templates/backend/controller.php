ca025bce149749d72677e48913dd28fb12084f36
oliverson1989@gmail.com <oliverson1989@gmail.com@1633c36a-21c8-e8cd-f5b8-5bfbea1bbbbd>
2013-09-23 07:52:19 +0000

    themer

--- trunk/ProtectedShop/modules/gii/generators/crud/templates/backend/controller.php
+++ trunk/ProtectedShop/modules/gii/generators/crud/templates/backend/controller.php
@@ -66,6 +66,42 @@ class <?php echo $this->controllerClass; ?> extends Backend<?php echo $this->bas
 			'model'=>$model,
 		));
 	}
+    /**
+    * Deletes more particular model.
+    * If deletion is successful, the browser will be redirected to the 'admin' page.
+    * @param integer $id the ID of the model to be deleted
+    */
+    public function actionDeleteCheck()
+    {
+        $_arr_id=$_POST['check_form'];
+        foreach($_arr_id as $id){
+        $this->loadModel($id)->delete();
+        }
+        $str_mess=strtr(Yii::t('backend_mess','notice.success'),
+        array('{text}'=>Yii::t('backend_mess','grid.delete.successful')));
+        echo $str_mess;
+        // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+        if(!isset($_GET['ajax']))
+        {
+        $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+        Yii::app()->user->setFlash('mess', $str_mess);
+    }
+
+    }
+    /**
+    * Page show change
+    */
+    public function actionPageShow()
+    {
+        $page_show=$_POST['page_show'];
+        $cookie = new CHttpCookie('page_show', $page_show);
+        $cookie->expire = time()+60*60*24*180;
+        Yii::app()->request->cookies['page_show'] = $cookie;
+
+        // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+        if(!isset($_GET['ajax']))
+        $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+    }
 
 	/**
 	 * Deletes a particular model.
--- trunk/ProtectedShop/modules/gii/generators/crud/templates/backend/index.php
+++ trunk/ProtectedShop/modules/gii/generators/crud/templates/backend/index.php
@@ -45,18 +45,70 @@ return false;
 </div>
 <div class="separator bottom"></div>
 
-<?php echo "<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>"; ?>
+<div class="innerLR">
+    <div class="widget">
+        <div class="widget-head"><h4 class="heading glyphicons list"><i></i><?php echo "<?php echo Yii::t('backend','title.list').\" \".Yii::t('backend','db.tbl$this->modelClass') ?>"?> </h4></div>
+        <div class="widget-body">
+            <div id="<?php echo $this->class2id($this->modelClass); ?>-grid-mess">
+                <?php echo "<?php echo Yii::app()->user->getFlash('mess'); ?>"?>
+            </div>
+            <!-- search-form -->
+            <?php echo "<?php $change_title=\"changeTitle('\".'<i></i>'.Yii::t('backend','btn.search.show').\"','\".'<i></i>'.Yii::t('backend','btn.search.hide').\"',this);\"; ?>"?>
+            <?php echo "<?php echo CHtml::link('<i></i>'.Yii::t('backend','btn.search.show'),'#',array('class'=>'glyphicons search search-button btn btn-small btn-primary','onclick'=>$change_title)); ?>"?>
+            <div class="separator bottom"></div>
+            <div class="search-form" style="display:none">
+                <div class="widget">
+                    <div class="widget-head"><h4 class="heading glyphicons search"><i></i><?php echo "<?php echo Yii::t('backend','title.search') ?>"?></h4></div>
+                    <div class="widget-body">
+                        <?php echo "<?php \$this->renderPartial('_search',array(
+	                        'model'=>\$model,
+                        )); ?>\n"; ?>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+            <!-- end search-form -->
+            <form name="frm_grid_<?php echo $this->class2id($this->modelClass); ?>" method="post" action="<?php Yii::app()->createUrl('<?php echo $this->class2id($this->modelClass); ?>/deleteSelected') ?>">
+<?php echo "<?php
+                $value_cookie = Yii::app()->request->cookies->contains('page_show') ?
+                    Yii::app()->request->cookies['page_show']->value : Yii::app()->params['grid.page_row_show'];
+                $params_option=Yii::app()->params['grid.array_option_page_show'];
+                $array_option_page_show='';
+                foreach($params_option as $value){
+                    $selected='';
+                    if($value==$value_cookie){
+                        $selected='selected=\"selected\"';
+                    }
+                    $array_option_page_show.='<option '.$selected.' value=\"'.$value.'\">'.$value.'</option>';
+                }
+                ?>"?>
 
-<div class="search-form" style="display:none">
-    <?php echo "<?php \$this->renderPartial('_search',array(
-	'model'=>\$model,
-)); ?>\n"; ?>
-</div><!-- search-form -->
-
-<?php echo "<?php"; ?> $this->widget('zii.widgets.grid.CGridView', array(
+<?php echo "<?php"; ?> $this->widget('application.modules.backend.extensions.widgets.AdminGridView', array(
 'id'=>'<?php echo $this->class2id($this->modelClass); ?>-grid',
 'dataProvider'=>$model->search(),
-'filter'=>$model,
+'pagerCssClass'=>'pagination pagination-small pull-right',
+'summaryCssClass'=>'separator bottom form-inline small',
+'ajaxUpdate'=>true,
+'emptyText'=>Yii::t('backend','grid.row_empty'),
+'urlPageShow'=>Yii::app()->createUrl('<?php echo $this->class2id($this->modelClass); ?>/pageShow'),
+'array_option_page_show'=>$array_option_page_show,
+'pager'=> array(
+'cssFile'=>false,
+'class'=>'application.modules.backend.extensions.widgets.LinkPager',
+'header'=>'<ul>',
+'footer'=>'</ul>',
+'nextPageLabel'=>'»',
+'prevPageLabel'=>'«',
+'htmlOptions'=>array(
+'class'=>'pagination pagination-small pull-right'),
+),
+'myPageSize'=>$value_cookie,
+'itemsCssClass'=>'table table-bordered table-condensed table-striped table-primary table-vertical-center table-fixed',
+'showNo'=>true,
+'showCheckBox'=>true,
+'valueFiledCheckBox'=>'<?php echo $this->tableSchema->primaryKey ?>',
+'nameForm'=>'frm_grid_<?php echo $this->class2id($this->modelClass); ?>',
+'afterAjaxUpdate'=>'function(){$("#<?php echo $this->class2id($this->modelClass); ?>-grid_table").fixedTable();}',
 'columns'=>array(
 <?php
 $count=0;
@@ -64,13 +116,43 @@ foreach($this->tableSchema->columns as $column)
 {
     if(++$count==7)
         echo "\t\t/*\n";
-    echo "\t\t'".$column->name."',\n";
+    echo "array('header'=>Yii::t('backend','db.tbl".$this->class2id($this->modelClass).".".$column->name."'),".
+                 "'type' => 'raw',".
+                 "'name'=>'password',".
+                 "'value'=>'CHtml::encode(\$data[\"".$column->name."\"])',),";
 }
 if($count>=7)
     echo "\t\t*/\n";
 ?>
 array(
-'class'=>'CButtonColumn',
+'class'=>'application.modules.backend.extensions.widgets.AdminCButtonColumn',
+'viewButtonOptions'=>array('class'=>'btn-action glyphicons eye_open btn-info'),
+'updateButtonOptions'=>array('class'=>'btn-action glyphicons pencil btn-success'),
+'deleteButtonOptions'=>array('class'=>'btn-action glyphicons remove_2 btn-danger'),
+'afterDelete'=>'function(control,flag,data){$("#<?php echo $this->class2id($this->modelClass); ?>-grid-mess").html(data)}',
+'htmlOptions'=>array('class'=>'center','style'=>'width: 100px;')
 ),
 ),
 )); ?>
+    <?php echo "<?php" ?>
+                if(Yii::app()->request->enableCsrfValidation)
+                {
+                    $csrfTokenName = Yii::app()->request->csrfTokenName;
+                    $csrfToken = Yii::app()->request->csrfToken;
+                    $csrf = ",{ '$csrfTokenName':'$csrfToken' }";
+                }
+                else
+                    $csrf = '';
+                ?>
+                <div style="width: 120px;" class="separator pull-left checkboxs_actions " >
+                    <div class="row-fluid">
+                        <select onchange="verifyCheck('<?php echo $this->class2id($this->modelClass); ?>-grid','frm_grid_<?php echo $this->class2id($this->modelClass); ?>','<?php echo "<?php"?> echo Yii::t('backend','alert.title') ?>','<?php echo "<?php"?> echo Yii::t('backend','grid.action.mess.confirm.delete') ?>','<?php echo "<?php" ?> echo Yii::t('backend','grid.not.row.select') ?>',this.value<?php echo "<?php"?> echo $csrf; ?>);" class="selectpicker span12" data-style="btn-default btn-small ">
+                            <option value="<?php echo "<?php" ?> echo Yii::app()->createUrl('<?php echo $this->class2id($this->modelClass); ?>/deleteCheck')?>"><?php echo "<?php" ?> echo Yii::t('backend','grid.action.delete') ?></option>
+                        </select>
+                    </div>
+                </div>
+                <div class="clearfix" style="clear: both; "></div>
+            </form>
+        </div>
+    </div>
+</div>
