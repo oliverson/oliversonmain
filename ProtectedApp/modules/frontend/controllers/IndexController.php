b21734e2ebeb34b8e6901bef96b74698a4fae24a
oliverson1989 <oliverson1989@gmail.com>
2014-06-26 16:50:14 +0000

    change app

---
+++ ProtectedApp/modules/frontend/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oliverson
+ * Date: 6/26/14
+ * Time: 8:24 PM
+ */
+class IndexController extends FrontendController
+{
+    public function actionIndex()
+    {
+        // renders the view file 'protected/views/site/index.php'
+        // using the default layout 'protected/views/layouts/main.php'
+        $this->render('index');
+    }
+}
