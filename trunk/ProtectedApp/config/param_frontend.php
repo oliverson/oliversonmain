d12ab7e633b56b8e5f45354a75a1a7b2ec5255b9
oliverson1989@gmail.com <oliverson1989@gmail.com@1633c36a-21c8-e8cd-f5b8-5bfbea1bbbbd>
2014-06-30 10:49:40 +0000

    test

---
+++ trunk/ProtectedApp/config/param_frontend.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: oliverson
+ * Date: 8/29/13
+ * Time: 11:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+return array(
+    "dir.upload"=>BASE_PATH."/public/upload/",
+);
--- trunk/ProtectedApp/modules/frontend/controllers/IndexController.php
+++ trunk/ProtectedApp/modules/frontend/controllers/IndexController.php
@@ -20,6 +20,29 @@ class IndexController extends FrontendController
         $this->render('Contact');
     }
     public  function actionGetArticle(){
-
+        $feed_class = new GetFeed();
+        $feed_class->feedUrl="http://vnexpress.net/rss/doi-song.rss";
+        $feed=$feed_class->getFeeds();
+        $item=$feed[0];
+        $str_description=$feed_class->getAsXMLContent($item->description);
+        $img=explode('src',$str_description);
+        if(isset($img[1])){
+            $img=explode('"',$img[1]);
+            if(isset($img[1])){
+                $img=explode('"',$img[1]);
+            }
+            $img=$img[0];
+            $rootPath = pathinfo(Yii::app()->request->scriptFile);
+            $file_name=explode("/",$img);
+            copy($img, $rootPath['dirname'].'/upload/'.$file_name[count($file_name)-1]);
+        }
+        $content = file_get_contents($item->link);
+        $title=explode('<div class="title_news">',$content);
+        $title=explode('</div>',$title[1]);
+        echo $title[0].Yii::app()->params["dir.upload"];
+        $detail=explode('<div class="fck_detail width_common">',$content);
+        $detail=explode('<div id="social_like"',$detail[1]);
+        $detail=explode('<script type="text/javascript">',$detail[0]);
+        echo "<div>".$detail[0];
     }
 }
